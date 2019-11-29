0ecc14a43a0414a385d8dffde68f4c64572175c2
cheloelian <57971657+cheloelian@users.noreply.github.com>
2019-11-28 22:49:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if("serviceWorker" in navigator) ///si navegador acepta serviceworker
 	{
 		console.log("Acepta service worker");
-		navigator.serviceWorker.register("/sw.js")
+		navigator.serviceWorker.register("sw.js")
 		.then((reg) => console.log("Service Worker Registrado",reg))
 		.catch((err) => console.log("Servide Worker NO Registrado",err));
 	}
