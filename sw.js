a86ca3182b932411a73edc11c59da9a43391cef3
cheloelian <57971657+cheloelian@users.noreply.github.com>
2019-12-03 21:24:10 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+// Instalar el service worker
+self.addEventListener("Install", evt =>{console.log("El Srvice Worker se instalo");});
+
+//Activar el service worker
+
+
+//Eventos Fetch - fetch request o pedido de busqueda
