7916468088b71a8f2275b12ff3cc9e5449d90506
cheloelian <57971657+cheloelian@users.noreply.github.com>
2019-12-03 21:40:25 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Instalar el service worker
-self.addEventListener("Install", evt =>{console.log("El Service Worker se instalo")});
+self.addEventListener("install", evt =>{console.log("El Service Worker se instalo");});
 
 //Activar el service worker
-
+self.addEventListener("activate", evt =>{console.log("El Service Worker se activo");});
 
 //Eventos Fetch - fetch request o pedido de busqueda
