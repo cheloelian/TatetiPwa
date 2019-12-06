56bf875384e755b5a047d2fad59b90526df0baf5
cheloelian <57971657+cheloelian@users.noreply.github.com>
2019-12-05 21:39:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,5 +18,14 @@ self.addEventListener("install", evt =>
 self.addEventListener("activate", evt =>{console.log("El Service Worker se activo");});
 
 //Eventos Fetch - fetch request o pedido de busqueda
-self.addEventListener("fetch", evt =>{console.log("Se Atrapo el Evento: ", evt);});
+self.addEventListener("fetch", evt =>{
+	console.log("Se atrapo el Evento: ", evt);
+	evt.respondWith(
+		caches.match(evt.request).then(cacheRes => {
+			return  cacheRes || fetch(evt.request)
+		})
+	);
+});
+
+
 
